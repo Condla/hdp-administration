6138457af97c48f016773a3bc08e9b72accb776f
Stefan K. Dunkler <stefan.dun@gmail.com>
2017-04-24 13:42:09 +0200

    add role adding script

---
+++ get_roles.sh
@@ -0,0 +1,10 @@
+export ANSIBLE_ROLES_DIR="./roles"
+
+mkdir $ANSIBLE_ROLES_DIR 
+cd $ANSIBLE_ROLES_DIR
+
+git clone git@github.com:Condla/hdp-instances.git
+git clone git@github.com:Condla/hadoop-bootstrap.git
+git clone git@github.com:Condla/hdp-mysql-server.git
+git clone git@github.com:Condla/hdp-ambari-agents.git
+git clone git@github.com:Condla/hdp-ambari-server.git
