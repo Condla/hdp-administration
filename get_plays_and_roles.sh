6ffc14c2f4c24c13cc065a9265988e93ee79e272
Stefan Kupstaitis-Dunkler <stefan.dun@gmail.com>
2017-12-10 11:58:59 +0100

    change ambari hostname assignment

--- get_plays_and_roles.sh
+++ get_plays_and_roles.sh
@@ -5,7 +5,7 @@ cd $ANSIBLE_ROLES_DIR
 git clone git@github.com:Condla/hdp-instances.git
 git clone git@github.com:Condla/hadoop-bootstrap.git
 git clone git@github.com:Condla/hdp-mysql-server.git
-git clone git@github.com:Condla/hdp-ambari-agents.git
+git clone git@github.com:Condla/hdp-ambari-agent.git
 git clone git@github.com:Condla/hdp-ambari-server.git
 git clone git@github.com:Condla/hdp-security.git
 git clone git@github.com:Condla/hdp-users.git
